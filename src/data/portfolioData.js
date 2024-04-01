f317a958754413dcf7da6b145aeb97da8c948b58
Sean-Cruickshank <seancruickshank2023@gmail.com>
2024-04-01 21:48:41 +1300

    Worked on Portfolio Details

--- src/data/portfolioData.js
+++ src/data/portfolioData.js
@@ -1,12 +1,15 @@
-import CSF from '../images/csf_logo.jpg'
+import CSF from '../images/coolspacefacts/csf_logo.jpg'
 import portrait from '../images/portrait.png'
+import { nanoid } from 'nanoid'
 
 export const portfolioData = [
   {
     id: "1",
     title: "Cool Space Facts",
-    description: "A website about space and planets and shit how long does this paragraph need to be",
-    year: "2023 - 2024",
+    downloadLink: 'coolspacefacts',
+    description: "A website about space, with different pages listing facts about various objects in space, ordered from smallest to largest. I tried to make the website as visually appealling as possible.",
+    yearShort: "2023 - 2024",
+    yearLong: "December 2023 - January 2024",
     tagsFront: [
       "HTML",
       "CSS",
@@ -17,13 +20,36 @@ export const portfolioData = [
       "css",
       "javascript"
     ],
-    thumbnail: CSF
+    thumbnail: CSF,
+    screenshotsFilesize: "24.3 MB",
+    screenshotsDescription: "23 images of the website at different pages and resolutions",
+    projectFilesize: "5.9 MB",
+    projectDescription: "Visual Studio project file",
+    pageText: [
+      <p key={nanoid()}>My main goal with making this website was to improve my knowledge of JavaScript. Primarily, it was to generate as much of the HTML on the website as possible, by reusing code.</p>,
+      <p key={nanoid()}>The website consists of four themed pages, where each page runs through a list of space objects ordered by that theme (size, distance, temperature, and speed). Information about each object is stored in a JavaScript array, where it is fed through a for loop to generate the content for the page.</p>,
+      <p key={nanoid()}>This website was also a good oppourtunity for me to learn more about content attribution and licencing. The website features an attributions page, where sources for all the images and facts used on the website can be found. All images were sourced through either public domain, or creative commons.</p>
+    ],
+    images: [
+      "https://d2ep14lbls2qwm.cloudfront.net/md_size.png",
+      "https://d2ep14lbls2qwm.cloudfront.net/md_distance.png",
+      "https://d2ep14lbls2qwm.cloudfront.net/md_temp.png",
+      "https://d2ep14lbls2qwm.cloudfront.net/md_speed.png"
+    ],
+    imageTitles: [
+      "A section about the International Space Station on the 'Size' page",
+      "A section about Mercury on the 'Distance' page",
+      "A section about Titan on the 'Temperature' page",
+      "A section about Mercury on the 'Speed' page"
+    ]
   },
   {
     id: "2",
     title: "Tenzies",
+    downloadLink: "tenzies",
     description: "A game",
-    year: "2024",
+    yearShort: "2024",
+    yearLong: "February 2024",
     tagsFront: [
       "HTML",
       "CSS",
@@ -34,13 +60,17 @@ export const portfolioData = [
       "css",
       "react"
     ],
-    thumbnail: portrait
+    thumbnail: portrait,
+    screenshotsFilesize: "",
+    projectFilesize: ""
   },
   {
     id: "3",
     title: "Shop",
+    downloadLink: "shop",
     description: "Buy things here",
-    year: "2024",
+    yearShort: "2024",
+    yearLong: "2024",
     tagsFront: [
       "HTML",
       "CSS",
@@ -51,7 +81,9 @@ export const portfolioData = [
       "css",
       "c-sharp"
     ],
-    thumbnail: portrait
+    thumbnail: portrait,
+    screenshotsFilesize: "",
+    projectFilesize: ""
   }
 ]
 
@@ -98,5 +130,37 @@ export const filterData = [
   {
     front: "Jade",
     back: "jade"
+  },
+*/
+
+/*
+
+  --- Project Template ---
+
+  {
+    id: "",
+    title: "",
+    downloadLink: "",
+    description: "",
+    yearShort: "",
+    yearLong: "",
+    tagsFront: [
+      "",
+      "",
+      ""
+    ],
+    tagsBack: [
+      "",
+      "",
+      ""
+    ],
+    thumbnail: ,
+    screenshotsFilesize: "",
+    projectFilesize: "",
+    pageText: [
+      <p key={nanoid()}></p>,
+      <p key={nanoid()}></p>,
+      <p key={nanoid()}></p>
+    ]
   },
 */
--- src/pages/PortfolioDetails.js
+++ src/pages/PortfolioDetails.js
@@ -2,8 +2,12 @@ import { Link, useLocation, useParams } from 'react-router-dom'
 import { FaArrowLeft } from "react-icons/fa";
 import toTop from '../utilities/toTop'
 import { portfolioData } from "../data/portfolioData"
+import React from 'react';
+import { IoIosArrowBack, IoIosArrowForward } from "react-icons/io";
 
 export default function PortfolioDetails() {
+  
+  const [position, setPosition] = React.useState(0)
   
   const location = useLocation();
   let returnSearchParam = "";
@@ -26,13 +30,92 @@ export default function PortfolioDetails() {
     )
   }
 
-  console.log(pageData)
+  let list = "";
+  pageData.tagsFront.forEach((tag) => {
+    list = list + ", " + tag;
+  })
+  const formattedList = list.substring(1)
+
+  //Checks whether download links are available or not
+  //Downloads table conditionally renders if downloadsList array contains at least one value
+  let downloadsList = []
+  {pageData.screenshotsFilesize && downloadsList.push(pageData.screenshotsFilesize)}
+  {pageData.projectFilesize && downloadsList.push(pageData.projectFilesize)}
+
+  
+  function galleryIncrease() {
+    if (position < pageData.images.length - 1) {
+      setPosition(prev => prev + 1)
+    } else {
+      setPosition(0)
+    }
+  }
+  function galleryDecrease() {
+    if (position > 0) {
+      setPosition(prev => prev - 1)
+    } else {
+      setPosition(pageData.images.length - 1)
+    }
+  }
   
   return (
     <div className="portfolio-details container">
       <Link onClick={toTop} to={`/portfolio${returnSearchParam}`}><FaArrowLeft /> Back to My Projects</Link>
       <div className='portfolio-details-body'>
         <h1>{pageData.title}</h1>
+        <div className='portfolio-details-subtitle'>
+          <p>{pageData.yearLong}</p>
+          <p>{formattedList}</p>
+        </div>
+
+        {pageData.images &&
+        <div className='gallery'>
+          <div className='gallery-body'>
+            <button className='gallery-button-back' onClick={galleryDecrease}><IoIosArrowBack /></button>
+            <button className='gallery-button-forward' onClick={galleryIncrease}><IoIosArrowForward /></button>
+            <p className='gallery-position'>{position + 1} of {pageData.images.length}</p>
+            <img
+              src={pageData.images[position]}
+              title={pageData.imageTitles[position]}
+            />
+          </div>
+        </div>
+        }
+
+        { downloadsList.length > 0 &&
+          <table className='portfolio-details-table'>
+            <thead>
+              <tr>
+                <td width="18%"></td>
+                <th width="33%">Description</th>
+                <th width="24%">File Size</th>
+                <th width="25%"></th>
+              </tr>   
+            </thead>
+            <tbody>
+              {pageData.screenshotsFilesize &&
+                <tr>
+                  <td>Screenshots</td>
+                  <th>{pageData.screenshotsDescription}</th>
+                  <th>{pageData.screenshotsFilesize}</th>
+                  <th><a href={`https://d2ep14lbls2qwm.cloudfront.net/downloads/${pageData.downloadLink}/screenshots.zip`} download>Download</a></th>
+                </tr>
+              }
+              {pageData.projectFilesize &&
+                <tr>
+                  <td>Full Project</td>
+                  <th>{pageData.projectDescription}</th>
+                  <th>{pageData.projectFilesize}</th>
+                  <th><a href={`https://d2ep14lbls2qwm.cloudfront.net/downloads/${pageData.downloadLink}/project.zip`} download>Download</a></th>
+                </tr>
+              }
+            </tbody>
+          </table>
+        }
+        <div className='portfolio-details-text'>
+          <h2>Why I made this website</h2>
+          {pageData.pageText}
+        </div>
       </div>
     </div>
   )
